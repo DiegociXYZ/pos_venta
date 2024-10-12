eb7257a0b1e9a584628fd0633b5f0ad641b1aa40
DiegociXYZ <diegoci268@gmail.com>
2024-10-12 12:12:33 -0400

    video 4 conectar a la base de datos

--- Config/Config.php
+++ Config/Config.php
@@ -1,4 +1,8 @@
 <?php
 const base_url = "https://localhost/pos_venta/";
-
+const host = "localhost";
+const user = "root";
+const pass = "";
+const db = "sistema";
+const charset = "charset=utf8"
 ?>
