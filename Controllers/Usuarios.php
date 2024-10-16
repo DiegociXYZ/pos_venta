ae448b0072185a20298690309388dc9a8daaff8c
DiegociXYZ <diegoci268@gmail.com>
2024-10-15 22:38:05 -0400

    FEAT: Listar usuarios , vid 6

--- Controllers/Usuarios.php
+++ Controllers/Usuarios.php
@@ -6,8 +6,26 @@ class Usuarios extends Controller{
     }
     public function index()
     {
-        $this ->views->getView($this, "index");
+        $data['cajas'] = $this->model->getCajas();
+        $this ->views->getView($this, "index", $data);
     }
+    public function listar() {
+        $data = $this->model->getUsuarios();
+        for ($i=0; $i < count($data); $i++) {
+            if ($data[$i]['estado'] == 1) {
+                $data[$i]['estado'] = '<span class="badge badge-success">Activo</span>';
+            }else{
+                $data[$i]['estado'] = '<span class="badge badge-danger">Inactivo</span>';
+            } 
+            $data[$i]['acciones'] = '<div>
+            <button class="btn btn-primary" type="button">Editar</button>
+            <button class="btn btn-danger" type="button">Eliminar</button>
+            </div>';
+        }
+        echo json_encode($data, JSON_UNESCAPED_UNICODE);
+        die();
+    }
+
     public function validar(){
         if (empty($_POST['usuario']) || empty($_POST['clave'])) {
             $msg = "Los campos estan vacios";
@@ -30,3 +48,4 @@ class Usuarios extends Controller{
 }
 
 ?>
+
--- Models/UsuariosModel.php
+++ Models/UsuariosModel.php
@@ -12,6 +12,22 @@ class UsuariosModel extends Query {
         $data = $this->select($sql);
         return $data;
     }
+
+    public function getCajas()
+    {
+        $sql = "SELECT * FROM caja WHERE estado = 1";
+        $this ->select($sql);
+        $data = $this->selectAll($sql);
+        return $data;
+    }
+
+    public function getUsuarios()
+    {
+        $sql = "SELECT u.*, c.id, c.caja FROM usuarios u INNER JOIN caja c WHERE u.id_caja = c.id";
+        $this ->select($sql);
+        $data = $this->selectAll($sql);
+        return $data;
+    }
 }
 
 ?>
